c4285bd04b4cf165435d06493120f6b66bc42628
鰐渕　航 <a77968@h088.goldwin.co.jp>
2023-01-23 22:19:52 +0900

    1st

---
+++ app.js
@@ -0,0 +1,13 @@
+for(let i = 0; i < 51; i++) {
+  console.log(i);
+  if(i % 10 == 0) {
+    console.log("今" + i + "回ループしました。");
+  }
+  if(i % 4 == 0) {
+     console.log("4で割れる数です。" + i);
+  }
+  if(i == 50) {
+    alert("50回カウント終わりました。");
+  }
+}
+
